e1f16a924e3650958c801dc47e7df9b8dbe1fd44
[Andrew Core] <[adcore89@gmail.com]>
2021-07-10 13:24:16 -0400

    contact form added, needs to be conditionally rendered, need error handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import ContactMe from './components/Contact';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <ContactMe></ContactMe>
         <About></About>
       </main>
     </div>
